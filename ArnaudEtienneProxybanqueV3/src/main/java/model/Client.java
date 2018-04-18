8f71d2023f022238e74f39be9e00ee761870cd4f
arnaud4810 <arnaud.renard.esilv@gmail.com>
2018-04-18 18:29:59 +0200

    Troisième Commit

---
+++ ArnaudEtienneProxybanqueV3/src/main/java/model/Client.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Client {
+private int idClient;
+
+public Client(int idClient) {
+	super();
+	this.idClient = idClient;
+}
+
+public int getIdClient() {
+	return idClient;
+}
+
+public void setIdClient(int idClient) {
+	this.idClient = idClient;
+}
+
+
+}
